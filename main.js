c1b5db5ac351f19b0bce041c7aa1146cb9ea8f56
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-25 17:16:07 +0100

    completo

--- main.js
+++ main.js
@@ -5,25 +5,45 @@ var app = new Vue({
       todos: [
         {
           title: 'Fare la spesa',
+          status: 'todo',
         },
         {
           title: 'Annaffiare le piante',
+          status: 'todo',
         },
         {
           title: 'Preparare la cena',
+          status: 'todo',
         },
         {
           title: 'Fare jogging',
+          status: 'todo',
         },
       ],
     },
+    computed: {
+      todosComputed: function () {
+        let todosDone = this.todos.filter[(todo) => todo.status == 'done'];
+        let todosTodo = this.todos.filter[(todo) => todo.status == 'todo'];
+        return [...todosTodo, ...todosDone];
+      }
+    },
     methods: {
-      add: function() {
-        let obj = {
-          title: this.inputText
+      add: function () {
+        if (this.inputText !='') {
+          let obj = {
+          title: this.inputText,
+          status: 'todo'
         }
         this.todos.push(obj);
+        this.inputText = '';
       }
-
+    },
+    check: function (index) {
+      this.todos[index].status = 'done';
+    },
+    remove: function (todo) {
+      let index = this.todos.indexOf(todo);
     }
-  });
+  }
+});
