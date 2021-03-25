4212f0e4134bc375ad7e43c115262a5c54b0633c
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-25 15:43:58 +0100

    add list

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      inputText: '',
+      todos: [
+        {
+          title: 'Fare la spesa',
+        },
+        {
+          title: 'Annaffiare le piante',
+        },
+        {
+          title: 'Preparare la cena',
+        },
+        {
+          title: 'Fare jogging',
+        },
+      ],
+    },
+    methods: {
+      add: function() {
+        let obj = {
+          title: this.inputText
+        }
+        this.todos.push(obj);
+      }
 
+    }
+  });
